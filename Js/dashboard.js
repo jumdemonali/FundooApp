389b3fe89940b3913a26cc4b249ab6b632aa98ce
jumdemonali <jumdemonali@gmail.com>
2022-01-25 20:31:43 +0530

    Added: dashboard note container colors

--- Js/dashboard.js
+++ Js/dashboard.js
@@ -10,7 +10,59 @@ window.addEventListener("DOMContentLoaded",function(){
   let notesContainer=document.querySelector(".notes-background");
   let archive=false;
   let archiveButton=document.querySelector("#Archive");
+  let colorpopupbtn=document.querySelector(".bi-palette");
+  let colorsOptions=document.querySelector(".clr");
+  let clrs="";
+
+
   
+
+  // $(document).on('click', '.clr', function (event) {
+//     color=event.target.id;
+// console.log(event.target.getAttribute("data-a"));
+//  })
+//  colorpopupbtn.addEventListener("mouseover",function(event){
+//   event.preventDefault()
+// mapColorToPopp("create");
+ 
+
+// })
+//  function mapColorToPopp(action){
+   
+//   let decideClass = ""
+//   if(action === "create"){
+//       decideClass="color"
+//   }
+//   else if(action ==="update"){
+//       decideClass="update"
+//   }
+//   console.log(decideClass)
+//   console.log(action)
+//   const colorArray=["#FFFFFF ","#FBBC04","#FFF475","#CCFF90","#A7FFEB","#CBF0F8","#AECBFA","#D7AEFB","#FDCFE8","#E6C9A8","#E8EAED","#D7AEFB"];
+ 
+//   colourContainer.innerHTML=colorArray.map((color)=>
+//   `<div class=${decideClass} style="background-color:${color}" id=${note.color} ></div>`).join('')
+
+//   console.log("hey From ColorArray")
+  
+// }
+
+
+//On clicking on a color that colour is given to the background of Main Take Note
+// $('body').on('click', '.clr', function(e) {
+//   e.preventDefault()
+//   console.log("hey From Color CLick")
+//   console.log("Color = "+e.currentTarget.id);
+//   colorValue=e.currentTarget.id;
+//   console.log(colorValue)
+//   title2.style.background=colorValue;
+//   titleDescription.style.background=colorValue;
+//   noteContainerTwo.style.background=colorValue;
+ 
+// })
+
+  
+
   noteContainerOne.addEventListener("click",function(e){
     e.stopPropagation();
     noteContainerOne.style.display="none";
@@ -26,9 +78,9 @@ window.addEventListener("DOMContentLoaded",function(){
       console.log(resp.data.data.data);
       let noteArray=resp.data.data.data;
       // notesContainer.innerHTML="abc";
-      notesContainer.innerHTML = noteArray.map((note) =>`<div class="noteBox">
-      <div>${note.title}</div><br>
-      <div>${note.description}</div>
+      notesContainer.innerHTML = noteArray.map((note) =>`<div class="noteBox" style="background-color:${note.color}">
+      <div class="smallBoxNote1">${note.title}</div><br>
+      <div class="smallBoxDesc1">${note.description}</div>
      <div class="logoSet"><i class="bi bi-bell"></i>
      <i class="bi bi-person-plus"></i>
       <a href="#" id="clrSelector" data-toggle="popover" data-html="true"> <i class="bi bi-palette"></i></a>
@@ -50,11 +102,13 @@ descriptionInput.addEventListener('change',function(e){
 })
 close.addEventListener('click',function(e){
   console.log(title,desc);
+  console.log(clrs);
   requirejs(['../Service/dataservices.js'], (methods) => {
    let obj={
      title:title,
      description:desc,
-     isArchived:archive
+     isArchived:archive,
+     color:clrs
    }
    methods.addNotes(JSON.stringify(obj)).then((resp)=>{
      console.log(resp);
@@ -63,6 +117,44 @@ close.addEventListener('click',function(e){
    })
 })
 })
+  
+    /*Delete Note And move to Trash*/
+    
+      
+      $('body').on('click', '.deleteButtonNote', function(e) {
+        e.preventDefault()
+        console.log("Hey Delete button inside note is clicked")
+        console.log(e.currentTarget.id)
+      
+        let object ={
+            noteIdList : [e.currentTarget.id],
+            "isDeleted": true,
+           
+    }
+    
+    console.log(object)
+    // let objectServer=JSON.stringify(object);
+    // console.log(objectServer)
+        requirejs(['../service/userService.js'], (methods) => {
+            methods.trashNote("http://fundoonotes.incubation.bridgelabz.com/api/notes/getTrashNotesList",JSON.stringify(object)).then(function(response){
+                
+                console.log(response)
+                
+                getNote()
+                let a = JSON.parse(response)
+                //  console.log(a.data.data)
+                  // console.log(a.data.data.id)
+       
+    
+               
+            }).catch(function(error){
+                console.log(error)
+            })
+        
+        })
+       
+     });
+    
 window.addEventListener('click', function(e){   
   if (document.querySelector('.note-container2').contains(e.target)){
     console.log("Clicked in box");
@@ -72,8 +164,12 @@ window.addEventListener('click', function(e){
   noteContainerTwo.style.display="none";
   }
 })
+$(document).ready(function(){
+  $('[data-toggle="popover"]').popover();   
+});
 const colors = ["#2ECC71","#AF7AC5","#F1948A","#A3E4D7","#F5B7B1","#F5B041","#DC7633","#F1C40F","#AAB7B8","#F1948A","#2ECC71","#F5B041"];
-let value=colors.map((color) =>`<div class="round-boxes" style="background-color:red" border:1px solid red";>Hello</div>`).join('');
-//  let value=`<div class="round-box" style="background-color: #92a8d1;"></div>`
+let value=colors.map((color) =>`<div id=${color}
+style="background-color:${color};  width:30px; height:25px; border-radius: 50%;
+  margin: 2px 2px 2px 2px;"></div>`).join('');
 document.getElementById("clrSelector").setAttribute("data-content",value);
 })  
